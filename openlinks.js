3c2598b88e3993a75d45eba09ecfa9cc03e1b01a
nein09 <nein09@nein09.com>
2017-01-12 17:13:42 -0800

    Make it easier to extend

--- openlinks.js
+++ openlinks.js
@@ -8,36 +8,29 @@ var currentBiennium = (function () {
 
 $(document).ready(function() {
 	$("#biennium")[0].textContent = $("#biennium")[0].value =  currentBiennium;
-	console.log($("#submitButton"));
 });
 
 function submit(){
 	var timeout = 10; // timeout incrementing lets us avoid some basic open-multiple-windows protections.
 	var hb = $("#billNumber")[0].value;
 	var dates = $("#biennium")[0].value;
-	
-	var digestURL = "https://lawfilesext.leg.wa.gov/biennium/" + dates + "/Htm/Digests/House/" + hb + ".DIG.htm";
-	setTimeout( function(){ window.open(digestURL, "digest") }, timeout);
-	timeout += 10;
-	
-	var fullURL = "https://lawfilesext.leg.wa.gov/biennium/" + dates + "/Htm/Bills/House%20Bills/" + hb + ".htm";
-	setTimeout( function(){ window.open(fullURL, "full") }, timeout);
-	timeout += 10;
-	
-	var docsURL = "https://app.leg.wa.gov/dlr/tld/results.aspx?params=" + dates + "," + hb;
-	setTimeout( function(){ window.open(docsURL, "docs") }, timeout);
-	timeout += 10;
-	
-	var statusURL = "https://app.leg.wa.gov/billsummary?BillNumber=" + hb + "&Year=" + dates.substring(0,4);
-	setTimeout( function(){ window.open(statusURL, "status") }, timeout);
-	timeout += 10;
 
-	if (dates === currentBiennium) {
-		var feedbackURL = "https://app.leg.wa.gov/pbc/bill/" + hb;
-		setTimeout( function(){ window.open(feedbackURL, "feedback") }, timeout);
-		timeout += 10;
+	var urls = {
+		"digest": "https://lawfilesext.leg.wa.gov/biennium/" + dates + "/Htm/Digests/House/" + hb + ".DIG.htm",
+		"fullUrl": "https://lawfilesext.leg.wa.gov/biennium/" + dates + "/Htm/Bills/House%20Bills/" + hb + ".htm",
+		"docs": "https://app.leg.wa.gov/dlr/tld/results.aspx?params=" + dates + "," + hb,
+		"status": "https://app.leg.wa.gov/billsummary?BillNumber=" + hb + "&Year=" + dates.substring(0,4),
+		"feedback": dates === currentBiennium ? "https://app.leg.wa.gov/pbc/bill/" + hb : ""
+	};
+
+	var openLink = function(title, url) {
+		window.open(url, title);
 	}
-	
-	
- 
+
+	for (var title in urls) {
+    if (urls.hasOwnProperty(title) && urls[title]) {
+      setTimeout(openLink(title, urls[title]), timeout);
+      timeout += 10;
+    }
+	}
 }
